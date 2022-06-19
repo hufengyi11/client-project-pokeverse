0e04b7824d4a944faebc2dbe8d412c23a49be21f
Haining Zhen <hainingzhen@outlook.com>
2022-06-19 14:38:58 +0100

    pokedex css transferred from figma

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import ScreenContainer from "./containers/ScreenContainer";
+import PokeDex from "./containers/PokeDex";
+// import './containers/pokedex.css';
+
 function App() {
   return (
     <>
-    <ScreenContainer/>
+    {/* <ScreenContainer/> */}
+    <PokeDex />
+    
     </>
   );
 }
---
+++ src/containers/PokeDex.js
@@ -0,0 +1,78 @@
+import "./pokedex.css";
+
+const PokeDex = () => {
+  return (
+    <section>
+      {/* <div className="v1_3"></div> */}
+      {/* <div className='v8_2'>
+                <div className='mainYellowContainer'></div>
+            </div> */}
+      <div className="shit-container">
+        <div className="mainYellowContainer">
+          <div>
+            <div className="screen-border-top">
+                <div className="screen-border-top-opaque"></div>
+            </div>
+          </div>
+
+          <div className="screen"></div>
+          <div className="screenBorderBottomText">POK&Eacute;VERSE&trade;</div>
+
+          <div className="screenBorderTopText">Team Rocket Edition</div>
+          <div className="v13_1212"></div>
+
+          <div className="buttonframe">
+            <div className="DPadVertical"></div>
+            <div className="DPadHorizontal"></div>
+            <div className="DPadCenter"></div>
+            <div className="Nintendo-container">
+              <div className="Nintendo">Nintendo</div>
+              <div className="Nintendo-outline"></div>
+            </div>
+            <div className="noButton">B</div>
+            <div className="yesButton">A</div>
+            <div className="BottomButtonContainer">
+              <div className="bottomButton1"></div>
+              <div className="bottomButton2"></div>
+            </div>
+            <div className="v13_1213"></div>
+          </div>
+          <div classname='v13_11'></div>  
+          <div className="v13_1213"></div>
+          <div className="v13_1099"></div>
+        </div>
+      </div>
+      {/* <div className='v8_5'></div> */}
+    </section>
+  );
+};
+
+export default PokeDex;
+
+// .v8_2 {
+//     width: 100vw;
+//     height: 100vh;
+//     /* background: url("../images/v8_2.png"); */
+//     background-color: transparent;
+//     background-repeat: no-repeat;
+//     background-position: center center;
+//     background-size: cover;
+//     opacity: 1;
+//     overflow: hidden;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     position: relative;
+//   }
+
+// .v1_3 {
+//     width: 428px;
+//     height: 926px;
+//     background: rgba(255, 255, 255, 1);
+//     opacity: 1;
+//     position: relative;
+//     top: 0px;
+//     left: 0px;
+//     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+//     overflow: hidden;
+//   }
