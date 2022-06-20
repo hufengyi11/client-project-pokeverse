eb87f2f3206c3d145281c86362f95ac0af2706e9
hufengyi11 <fengyihu456@gmail.com>
2022-06-20 11:17:59 +0100

    Added desktop svg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import ScreenContainer from "./containers/ScreenContainer";
-import PokeDex from "./containers/PokeDex";
-// import './containers/pokedex.css';
+import { Component } from 'react';
+import './App.css';
+import PokeDex from './containers/PokeDex';
+import PokeDexDesktop from './containers/PokeDex-Desktop';
 
-function App() {
-  return (
-    <>
-    {/* <ScreenContainer/> */}
-    <PokeDex />
-    
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { matches: window.matchMedia("(min-width: 768px)").matches };
+  }
+
+  componentDidMount() {
+    const handler = e => this.setState({matches: e.matches});
+    window.matchMedia("(min-width: 768px)").addEventListener('change', handler);
+  }
+
+  render() {
+    return (
+      <div >
+        {this.state.matches && (<PokeDexDesktop />)}
+        {!this.state.matches && (<PokeDex/>)}
+      </div>
+    );
+  }
 }
 
 export default App;
