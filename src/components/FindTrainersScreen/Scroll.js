5a52d8e42701094a95bed02a7941bd3d1db533b7
hufengyi11 <fengyihu456@gmail.com>
2022-06-21 16:13:33 +0100

    clear the files and restart

--- src/components/FindTrainersScreen/Scroll.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Scroll() {
-  return (
-    <div>
-        
-    </div>
-  )
-}
--- src/components/FindTrainersScreen/Trainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Trainer() {
-  return (
-    <div>
-        <p>{Trainer.name}</p>
-    </div>
-  )
-}
