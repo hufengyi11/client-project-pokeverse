406fae824fbbc697a2f5fe2a2bf1382e12dad933
hufengyi11 <fengyihu456@gmail.com>
2022-06-22 10:42:51 +0100

    errors

--- src/components/AddNewTrainer/AddNewTrainer.js
+++ src/components/AddNewTrainer/AddNewTrainer.js
@@ -3,12 +3,13 @@ import './AddNewTrainer.css'
 import PostTrainer from '../APIfunctions/PostTrainer';
 import ButtonFunctions from '../ButtonFunctions';
 import axios from "axios";
+import { useState} from 'react';
 
 const AddNewTrainerScreen = ({ goToHomeScreen_FromAddNewTrainerScreen, handleButtonClick }) => {
 
-    const [trainerName, setTrainerName] = React.useState("");
-    const [trainerAge, setTrainerAge] = React.useState(0);
-    const [trainerTown, setTrainerTown] = React.useState("");
+    const [trainerName, setTrainerName] = useState("");
+    const [trainerAge, setTrainerAge] = useState(0);
+    const [trainerTown, setTrainerTown] = useState("");
 
     const handleNameChange = event => setTrainerName(event.target.value);
     const handleAgeChange = event => setTrainerAge(event.target.value);
@@ -17,43 +18,60 @@ const AddNewTrainerScreen = ({ goToHomeScreen_FromAddNewTrainerScreen, handleBut
     const handleSubmit = (event) => {
         event.preventDefault();
         const trainer = {
-            "trainerName":trainerName,
-            "trainerAge":trainerAge,
-            "trainerTown":trainerTown
+            name: trainerName,
+            age: trainerAge,
+            town: trainerTown,
+            pokemons: [],
+            pokemonCount:0
         }
-        axios.post('http://localhost:8080/trainer', trainer)
+        // axios({
+        //     method: 'post',
+        //     url: 'http://localhost:8080/trainer',
+        //     data: {
+        //         "trainerName": trainerName,
+        //         "trainerAge": trainerAge,
+        //         "trainerTown": trainerTown
+        //     }
+        //   });
+        axios.post('http://localhost:8080/trainer', { trainer })
             .then(res => {
                 console.log(res);
             }).catch((err) => console.log(err));
+
+        console.log("added");
     }
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <div>Add New Trainer</div>
-                <div>Name:</div>
-                <input
-                    type="text"
-                    className="add-new-trainer-name-input"
-                    value={trainerName}
-                    onChange={handleNameChange}
-                />
-                <div>Age:</div>
-                <input
-                    type="text"
-                    className="add-new-trainer-age-input"
-                    value={trainerAge}
-                    onChange={handleAgeChange}
-                />
-                <div>Town:</div>
-                <input
-                    spellCheck="false"
-                    type="text"
-                    className="add-new-trainer-town-input"
-                    value={trainerTown}
-                    onChange={handleTownChange}
-                />
-                <input type="submit" className='submitButton' value="submit" />
+            <div>Add New Trainer</div>
+            <form>
+                <label className="label">
+                    Name:
+                    <input
+                        type="text"
+                        onChange={handleNameChange}
+                        className="label"
+                    />
+                </label>
+                <label className="label">
+                    Age:
+                    <input
+                        type="number"
+                        onChange={handleAgeChange}
+                        className="label"
+                    />
+                </label>
+                <label className="label">
+                    Town:
+                    <input
+                        spellCheck="true"
+                        type="text"
+                        onChange={handleTownChange}
+                        className="label"
+                    />
+                    <label className="label">Press A to submit</label>
+                    <button className="yesButton-invisible" type='submit' onClick={handleSubmit}></button>
+                </label>
             </form>
             <ButtonFunctions
                 goToHomeScreen_FromAddNewTrainerScreen={goToHomeScreen_FromAddNewTrainerScreen}
