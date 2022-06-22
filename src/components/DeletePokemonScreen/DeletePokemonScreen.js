deb0d95fd0ffeffdf3dc36962b8ce4a63318c592
Lambert-Simnel <Josephsut@gmail.com>
2022-06-22 13:52:16 +0100

    Delete Pokemon Screen added without css

---
+++ src/components/DeletePokemonScreen/DeletePokemonScreen.js
@@ -0,0 +1,40 @@
+import ButtonFunctions from "../ButtonFunctions"
+import { useState } from "react";
+import "./DeletePokemoncreen.css"
+import DeletePokemon from "../APIfunctions/DeletePokemon";
+
+
+const DeletePokemonScreen = ({ goToHomeScreen_FromDeletePokemonScreen }) => {
+
+
+    const [pokemonId, setPokemonId] = useState([]);
+
+    
+    return (
+        <>
+
+
+            <form>
+                <label htmlFor="name">Delete Pokemon by ID</label>
+                <input type='text'onChange={(event) => {
+                    setPokemonId(event.target.value)
+                }}/>
+            </form>
+
+            <button 
+                className="yesButton-invisible" 
+                onClick={() => {DeletePokemon(pokemonId)}}
+            ></button>
+
+
+            <ButtonFunctions
+                goToHomeScreen_FromDeletePokemonScreen={goToHomeScreen_FromDeletePokemonScreen}
+                screen={'DeletePokemonScreen'}
+            />
+        </>
+
+    )
+
+}
+
+export default DeletePokemonScreen
