1722014289846970f436e053531e9fb6c0ae9124
hufengyi11 <fengyihu456@gmail.com>
2022-06-22 14:23:28 +0100

    completed delete trainer screen

--- src/components/DeleteTrainers/DeleteTrainersScreen.js
+++ src/components/DeleteTrainers/DeleteTrainersScreen.js
@@ -15,19 +15,15 @@ const DeleteTrainersScreen = ({ goToHomeScreen_FromDeleteTrainersScreen }) => {
                 setTrainers(trainers);
             }).catch((err) => console.log(err));
     }, [])
-
-    useEffect(() => {
-        axios.delete('http://localhost:8080/trainer')
-            .then(res => {
-                const trainers = res.data;
-                setTrainers(trainers);
-            }).catch((err) => console.log(err));
-    }, [])
-
-    function handleDeleteTrainer() {
-        console.log("begin")
-        const newTrainers = trainers.filter(trainer => trainer.id !== searchField)
-        setTrainers(newTrainers)
+   
+    function handleDeleteTrainer(e) {
+        e.preventDefault();        
+        axios.delete(`http://localhost:8080/trainer/${searchField}`)
+        .then(res => {
+            // const trainers = res.data;
+            // setTrainers(trainers);
+        }).catch((err) => console.log(err));
+        setSearchField(0);
     }
 
     return (
@@ -38,14 +34,14 @@ const DeleteTrainersScreen = ({ goToHomeScreen_FromDeleteTrainersScreen }) => {
                 <input
                     className="label"
                     type="number"
+                    value={searchField}
                     onChange={(event) => { setSearchField(event.target.value) }}
                 />
                 <button className="yesButton-invisible" onClick={handleDeleteTrainer}></button>
             </form>
 
             {trainers.filter((val) => {
-                if (searchField == "") { return null }
-                else if (val.id == searchField) {
+                if (val.id == searchField) {
                     return val
                 }
             }).map((val, key) => {
